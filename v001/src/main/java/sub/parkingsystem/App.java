cdc69175e29c01e091efecb36a89116f83275918
shinji-sub <wltjq2006@naver.com>
2019-12-18 18:17:54 +0900

    .

---
+++ v001/src/main/java/sub/parkingsystem/App.java
@@ -0,0 +1,10 @@
+// 프로젝트 : 주차장 입·출차 시스템
+//
+package sub.parkingsystem;
+
+public class App {
+   
+   public static void main(String[] args) {
+        System.out.println("주차 관리 시스템에 오신걸 환영합니다!");
+    }
+}
