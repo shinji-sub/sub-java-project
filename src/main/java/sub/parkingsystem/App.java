90c2cc37f03a8b3a40b47af7af90281f090bde4e
shinji-sub <wltjq2006@naver.com>
2019-12-18 18:58:41 +0900

    변수와 키보드 입력 받기

--- src/main/java/sub/parkingsystem/App.java
+++ src/main/java/sub/parkingsystem/App.java
@@ -1,10 +1,15 @@
-// 프로젝트: 주차장 입·출차 시스템
-//
 package sub.parkingsystem;
 
 public class App {
-   
-   public static void main(String[] args) {
-        System.out.println("주차 관리 시스템에 오신걸 환영합니다!");
-    }
+
+  public static void main(String[] args) {
+    System.out.println("번호: 1");
+    System.out.println("수업명: 자바 프로젝트 실습");
+    System.out.println("설명: 자바 프로젝트를 통한 자바 언어 활용법 익히기");
+    System.out.println("시작일: 2019-01-02");
+    System.out.println("종료일: 2019-05-28");
+    System.out.println("총수업시간: 1000 시간");
+    System.out.println("일수업시간: 8 시간");
+
+  }
 }
