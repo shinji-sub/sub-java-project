48dccda97d4a4170da58d2c02417ec7ee2544a35
shinji-sub <wltjq2006@naver.com>
2019-12-18 20:51:11 +0900

    EX02 작업

---
+++ src/main/java/sub/Snippet.java
@@ -0,0 +1,12 @@
+package sub;
+
+public class Snippet {
+  public static void main(String[] args) {
+    package sub.parkingsystem;
+  }
+
+  public static void main(String[] args) {
+    package sub.parkingsystem;
+  }
+}
+
