f0c871afbcf526820095ed19428c66c328e0a0e3
shinji-sub <wltjq2006@naver.com>
2019-12-27 16:56:37 +0900

    v011 생성자 필요한 이유

---
+++ v011/src/main/java/sub/domain/Board.java
@@ -0,0 +1,11 @@
+package sub.domain;
+
+import java.sql.Date;
+
+  public class Board{
+    public int no;
+    public String title;
+    public Date date;
+    public int viewCount;
+  }
+
---
+++ v011/src/main/java/sub/domain/Customer.java
@@ -0,0 +1,19 @@
+package sub.domain;
+
+public class Customer {
+ 
+    
+      public int no;
+      public String Cartype;
+      public String CarNumbel;
+      public String photo;
+      public String DiscountRatr;
+      public String Payment;
+      public String GyeoljeYuhyeong;
+      public String parking;
+      public String ExitStatus;
+      public String acceptance;
+    }
+
+
+
