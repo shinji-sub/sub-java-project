74424ec0041956bc5a28b6372fb950274395545e
shinji-sub <wltjq2006@naver.com>
2020-01-16 18:00:38 +0900

    v021_1 Iterator 디자인 패턴의 활용

---
+++ src/main/java/sub/util/ListIterator.java
@@ -0,0 +1,20 @@
+package sub.util;
+
+public class ListIterator<E> implements Iterator<E> {
+
+  List<E> list;
+  int cursor;
+
+  public ListIterator(List <E> list) {
+    this.list = list;
+  }
+  @Override
+  public boolean hasNext() {
+    return cursor < list.size();
+  }
+
+  @Override
+  public E next() {
+    return list.get(cursor++);
+  }
+}
