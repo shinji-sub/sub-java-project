480e0197401599ce8d0884972572a29372af5422
shinji-sub <wltjq2006@naver.com>
2020-02-20 19:47:37 +0900

    인터페이스 이용하여 DAO 호출 규칙을 통일하기

---
+++ sub-java-project-server/v029_10/src/main/java/sub/lms/servlet/BoardUpdateServlet.java
@@ -0,0 +1,29 @@
+package sub.lms.servlet;
+
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import sub.lms.dao.BoardDao;
+import sub.lms.domain.Board;
+
+public class BoardUpdateServlet implements Servlet {
+
+  BoardDao boardDao;
+
+  public BoardUpdateServlet(BoardDao boardDao) {
+    this.boardDao = boardDao;
+  }
+
+  @Override
+  public void service(ObjectInputStream in, ObjectOutputStream out) throws Exception {
+    Board board = (Board) in.readObject();
+
+
+    if (boardDao.update(board) > 0) {
+
+      out.writeUTF("OK");
+    } else {
+      out.writeUTF("FAIL");
+      out.writeUTF("해당 번호의 게시물이 없습니다.");
+    }
+  }
+}
